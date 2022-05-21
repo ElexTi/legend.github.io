82020c4d114c17ac77952ded5c902f48372fd535
katikn <antoshapetlin@gmail.com>
2022-05-21 20:55:37 +0700

    п

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.use(express.static(path.join(__dirname + '/public/')))
 app.get('/', (req, res) => {
